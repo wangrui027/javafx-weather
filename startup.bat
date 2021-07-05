052fd299a984b6608d8a389d7986700bf4f781c3
王睿 <wangrui1066@geostar.com.cn>
2021-07-05 10:12:43 +0800

    启动脚本调整

--- startup.bat
+++ startup.bat
@@ -1,3 +1,3 @@
 set JAVA_HOME=D:\Program Files\java\jdk1.8.0_291
 set path=%path%;%JAVA_HOME%\bin
-java -jar out\artifacts\javafx_weather\javafx_weather.jar
+java -jar target\javafx-weather-1.0-SNAPSHOT.jar
